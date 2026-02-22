823b619d06db69a554c49e83d33c2fc64e312470
TET ELITE <5hibalord69@gmail.com>
2026-02-22 21:11:34 +0700

    Initialize repo

---
+++ lib/1_color_app/color_app.dart
@@ -0,0 +1,153 @@
+import 'package:flutter/material.dart';
+
+final colorService = ColorService();
+
+void main() {
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Home(),
+    ),
+  );
+}
+
+
+enum CardType { red, blue }
+
+class ColorService extends ChangeNotifier {
+  int redTapCount = 0;
+  int blueTapCount = 0;
+
+  void tapRed() {
+    redTapCount++;
+    notifyListeners();
+  }
+
+  void tapBlue() {
+    blueTapCount++;
+    notifyListeners();
+  }
+}
+
+
+
+class Home extends StatefulWidget {
+  const Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  int _currentIndex = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: _currentIndex == 0
+          ? const ColorTapsScreen()
+          : const StatisticsScreen(),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentIndex,
+        onTap: (index) => setState(() => _currentIndex = index),
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.tap_and_play),
+            label: 'Taps',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.bar_chart),
+            label: 'Statistics',
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class ColorTapsScreen extends StatelessWidget {
+  const ColorTapsScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Color Taps')),
+      body: const Column(
+        children: [
+          ColorTap(type: CardType.red),
+          ColorTap(type: CardType.blue),
+        ],
+      ),
+    );
+  }
+}
+
+class ColorTap extends StatelessWidget {
+  final CardType type;
+
+  const ColorTap({super.key, required this.type});
+
+  Color get backgroundColor => type == CardType.red ? Colors.red : Colors.blue;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListenableBuilder(
+      listenable: colorService,
+      builder: (context, _) {
+        final tapCount = type == CardType.red
+            ? colorService.redTapCount
+            : colorService.blueTapCount;
+
+        return GestureDetector(
+          onTap: type == CardType.red ? colorService.tapRed : colorService.tapBlue,
+          child: Container(
+            margin: const EdgeInsets.all(10),
+            decoration: BoxDecoration(
+              color: backgroundColor,
+              borderRadius: BorderRadius.circular(10),
+            ),
+            width: double.infinity,
+            height: 100,
+            child: Center(
+              child: Text(
+                'Taps: $tapCount',
+                style: const TextStyle(fontSize: 24, color: Colors.white),
+              ),
+            ),
+          ),
+        );
+      },
+    );
+  }
+}
+
+class StatisticsScreen extends StatelessWidget {
+  const StatisticsScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Statistics')),
+      body: ListenableBuilder(
+        listenable: colorService,
+        builder: (context, _) {
+          return Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  'Red Taps: ${colorService.redTapCount}',
+                  style: const TextStyle(fontSize: 24, color: Colors.red),
+                ),
+                Text(
+                  'Blue Taps: ${colorService.blueTapCount}',
+                  style: const TextStyle(fontSize: 24, color: Colors.blue),
+                ),
+              ],
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
